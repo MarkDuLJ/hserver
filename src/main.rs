6c2b6c09d005804012d82c1a0f31f2273e4ba6f2
Mark Du <dulongjiebackup@gmail.com>
2024-08-04 17:22:30 -0400

    send back right response with a html file

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
-use std::{io::{BufRead, BufReader}, net::{TcpListener, TcpStream}};
+use std::{fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8787").unwrap();
+fn main(){
+    let listner = TcpListener::bind("127.0.0.1:8787").unwrap();
 
-    for stream in listener.incoming(){
+    for stream in listner.incoming() {
         let stream = stream.unwrap();
-
         handle_conn(stream);
     }
-
 }
 
 fn handle_conn(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_req: Vec<_> = buf_reader
-            .lines()
-            .map(|res| res.unwrap())
-            .collect();
 
-    println!("request: {:#?}", http_req);
+    let http_req: Vec<_> = buf_reader
+        .lines()
+        .map(|res| res.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let len = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {len}\r\n\r\n{contents}");
+    
+    stream.write_all(response.as_bytes()).unwrap();
+
+    println!("Request: {http_req:#?}");
 }
