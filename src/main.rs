5ea9a838fa0b69a82c2e312df8181f76e561567f
Mark Du <dulongjiebackup@gmail.com>
2024-08-05 09:51:27 -0400

    single thread to multi

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use std::{fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
+use std::{fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}, thread, time::Duration};
 
 fn main(){
     let listner = TcpListener::bind("127.0.0.1:8787").unwrap();
 
     for stream in listner.incoming() {
         let stream = stream.unwrap();
-        handle_conn(stream);
+
+        thread::spawn(|| {
+            handle_conn(stream);
+        });
+        
     }
 }
 
@@ -13,9 +17,13 @@ fn handle_conn(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {("HTTP/1.1 200 OK","hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
     
     let contents = fs::read_to_string(filename).unwrap();
