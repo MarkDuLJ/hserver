5ce170a05dde9f2eda9bfebb0b665e27b86f9be5
Mark Du <dulongjiebackup@gmail.com>
2024-08-05 14:33:37 -0400

    incoming()

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main(){
     // fixed thread pool to avoid bad behavier like ddos
 
     let pool = ThreadPool::new(4);
-    for stream in listner.incoming {
+    for stream in listner.incoming() {
         let stream = stream.unwrap();
 
         pool.excute(|| {
